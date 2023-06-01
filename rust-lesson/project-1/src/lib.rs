e3335e99ac937df9a87f36abb89068c8f98faa63
evpeople <hangcaihui@gmail.com>
2023-06-02 00:04:26 +0800

    finish project-1 part4, implement KvStore

--- rust-lesson/project-1/src/lib.rs
+++ rust-lesson/project-1/src/lib.rs
@@ -1,16 +1,24 @@
-pub struct KvStore {}
+use std::collections::HashMap;
+
+pub struct KvStore {
+    map: HashMap<String, String>,
+}
 
 impl KvStore {
     pub fn new() -> KvStore {
-        unimplemented!()
+        KvStore {
+            map: HashMap::new(),
+        }
     }
-    pub fn set(&mut self, _key: String, _value: String) {
-        unimplemented!()
+    pub fn set(&mut self, key: String, value: String) {
+        self.map.insert(key, value);
+        ()
     }
-    pub fn get(&mut self, _key: String) -> Option<String> {
-        unimplemented!()
+    pub fn get(&mut self, key: String) -> Option<String> {
+        self.map.get(&key).cloned()
     }
-    pub fn remove(&mut self, _key: String) {
-        unimplemented!()
+    pub fn remove(&mut self, key: String) {
+        self.map.remove(&key);
+        ()
     }
 }
